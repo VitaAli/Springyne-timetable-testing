cfac03a1b458efb7dbf58d7a1b3910cedf6db47f
Vitalija Ališauskienė <vitalija.alisauskiene@stud.vtmc.lt>
2023-02-21 19:56:04 +0200

    test for modules page

--- src/main/java/it/academy/pom/modules/ModulesPage.java
+++ src/main/java/it/academy/pom/modules/ModulesPage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ModulesPage {
 
     @FindBy (css = "#search-name-input")
@@ -11,6 +14,9 @@ public class ModulesPage {
 
     @FindBy (css = "button[type='submit']")
     private WebElement buttonEnter;
+
+    @FindBy (css = "tr td:nth-child(2)")
+    private List<WebElement> modulesList;
 
     public ModulesPage(WebDriver driver) {
         super();
@@ -22,6 +28,14 @@ public class ModulesPage {
 
     public void pressButtonEnter() {
         buttonEnter.click();
+    }
+
+    public List getModulesList() {
+        ArrayList<String> moduleNames = new ArrayList<>();
+        for (int index = 0; index < modulesList.size(); index++) {
+            moduleNames.add(modulesList.get(index).getText());
+        }
+        return moduleNames;
     }
 
 }
--- src/test/java/it/academy/test/modules/ModulesPageTest.java
+++ src/test/java/it/academy/test/modules/ModulesPageTest.java
@@ -2,23 +2,27 @@ package it.academy.test.modules;
 
 import it.academy.pom.Header;
 import it.academy.pom.modules.ModulesPage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import static it.academy.test.BaseTest.driver;
 
 public class ModulesPageTest {
 
     private Header header;
-    private ModulesPage moduleListPage;
+    private ModulesPage modulesPage;
 
+    @Test
     public void modulesCanBeFilteredByModuleName() {
 
         header = new Header(driver);
-        moduleListPage = new ModulesPage(driver);
+        modulesPage = new ModulesPage(driver);
 
         header.openModules();
-        moduleListPage.searchModuleByName("Test name1");
-        moduleListPage.pressButtonEnter();
+        modulesPage.searchModuleByName("Informacinių sistemų projektavimas");
+        modulesPage.pressButtonEnter();
 
+        Assertions.assertTrue(modulesPage.getModulesList().contains("Informacinių sistemų projektavimas"));
 
     }
 }
