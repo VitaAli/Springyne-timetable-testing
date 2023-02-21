3847c48a20e145301951cf1d22a5b9138e1d8084
Inga Saveljevaite <inga.saveljevaite@gmail.com>
2023-02-22 00:50:52 +0200

    RoomsPageTest initiated

--- src/main/java/it/academy/pom/rooms/RoomsPage.java
+++ src/main/java/it/academy/pom/rooms/RoomsPage.java
@@ -4,13 +4,64 @@ import it.academy.pom.AbstractPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RoomsPage extends AbstractPage {
 
     @FindBy (css = "a[href='#/rooms/create']")
-    private WebElement buttonAddRoom;
+    private WebElement buttonAddRooms ;
+
+    @FindBy (xpath = "(//input[@type='text'])[1]")
+    private WebElement inputByName;
+    @FindBy (xpath = "//div[@class='mx-3']//div[2]//form[1]//button[1]")
+    private WebElement buttonSearchByName;
+
+    @FindBy (xpath = "(//input[@type='text'])[2]")
+    private WebElement inputByBuilding;
+    @FindBy (xpath = "//div[@class='mx-3']//div[1]//form[1]//button[1]")
+    private WebElement buttonSearchByBuilding;
+
+    @FindBy (css = "tr td:nth-child(1)")
+    private List<WebElement> roomsListByName;
+
+    @FindBy (css = "tr td:nth-child(2)")
+    private List<WebElement> roomsListByBuilding;
 
     public RoomsPage(WebDriver driver) {
         super(driver);
     }
-}
+    public void searchRoomsByName(String name) {
+        inputByName.sendKeys(name);
+    }
+
+    public void pressButtonSearchByName() {
+        buttonSearchByName.click();
+    }
+
+    public List getRoomsListByName() {
+        ArrayList<String> roomsName = new ArrayList<>();
+        for (int index = 0; index < roomsListByName.size(); index++) {
+            roomsName.add(roomsListByName.get(index).getText());
+        }
+        return roomsName;
+    }
+
+    public void searchRoomsByBuilding(String name) {
+        inputByBuilding.sendKeys(name);
+    }
+
+    public void pressButtonSearchByBuilding() {
+        buttonSearchByBuilding.click();
+    }
+
+    public List getRoomsListByBuilding() {
+        ArrayList<String> roomsBuilding = new ArrayList<>();
+        for (int index = 0; index < roomsListByBuilding.size(); index++) {
+            roomsBuilding.add(roomsListByBuilding.get(index).getText());
+        }
+        return roomsBuilding;
+    }
+
+
+    }
---
+++ src/test/java/it/academy/test/Rooms/RoomsPageTest.java
@@ -0,0 +1,38 @@
+package it.academy.test.Rooms;
+
+import it.academy.pom.Header;
+import it.academy.pom.rooms.RoomsPage;
+import it.academy.test.BaseTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RoomsPageTest extends BaseTest {
+    private Header header;
+    private RoomsPage roomsPage;
+
+    @Test
+    public void roomsCanBeFilteredByRoomsName() {
+
+        header = new Header(driver);
+        roomsPage = new RoomsPage(driver);
+
+        header.openRooms();
+        roomsPage.searchRoomsByName("101");
+        roomsPage.pressButtonSearchByName();
+
+        Assertions.assertTrue(roomsPage.getRoomsListByName().contains("101"));
+
+        }
+    @Test
+    public void roomsCanBeFilteredByRoomsBuilding() {
+
+        header = new Header(driver);
+        roomsPage = new RoomsPage(driver);
+
+        header.openRooms();
+        roomsPage.searchRoomsByBuilding("Lakūnų g. 3, LT-09108 Vilnius");
+        roomsPage.pressButtonSearchByBuilding();
+
+        Assertions.assertTrue(roomsPage.getRoomsListByBuilding().contains("Lakūnų g. 3, LT-09108 Vilnius"));
+    }
+}
