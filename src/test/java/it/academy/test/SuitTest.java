5055ec66b9289fdc7b8b3fd3617028c59d4775f4
Vitalija Ališauskienė <vitalija.alisauskiene@stud.vtmc.lt>
2023-03-03 13:26:46 +0200

    suit test update

---
+++ src/test/java/it/academy/test/SuitTest.java
@@ -0,0 +1,20 @@
+package it.academy.test;
+
+import it.academy.test.modules.ModuleAddPageTest;
+import it.academy.test.modules.ModuleEditPageTest;
+import it.academy.test.modules.ModuleViewPageTest;
+import it.academy.test.modules.ModulesPageTest;
+import it.academy.test.rooms.RoomAddPageTest;
+import it.academy.test.rooms.RoomsPageTest;
+import it.academy.test.shifts.ShiftAddPageTest;
+import it.academy.test.shifts.ShiftEditPageTest;
+import it.academy.test.shifts.ShiftsPageTest;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ModulesPageTest.class, ModuleAddPageTest.class, ModuleEditPageTest.class, ModuleViewPageTest.class
+        , RoomsPageTest.class, RoomAddPageTest.class
+        , ShiftsPageTest.class, ShiftAddPageTest.class, ShiftEditPageTest.class })
+public class SuitTest {
+}
