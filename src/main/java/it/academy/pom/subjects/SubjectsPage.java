ee986f68f46764d4e86b63be85fc8586ad3da7d8
Vitalija Ališauskienė <vitalija.alisauskiene@stud.vtmc.lt>
2023-03-07 12:52:21 +0200

    subjects package adding, subjects page adding

---
+++ src/main/java/it/academy/pom/subjects/SubjectsPage.java
@@ -0,0 +1,13 @@
+package it.academy.pom.subjects;
+
+import it.academy.pom.AbstractPage;
+import org.openqa.selenium.WebDriver;
+
+public class SubjectsPage extends AbstractPage {
+
+    
+
+    public SubjectsPage(WebDriver driver) {
+        super(driver);
+    }
+}
