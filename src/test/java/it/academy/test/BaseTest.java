9903a22c9161f0dd847cb71c2ddd38c85593bad8
Vitalija Ališauskienė <vitalija.alisauskiene@stud.vtmc.lt>
2023-03-16 15:33:43 +0200

    saucelab setup

--- src/test/java/it/academy/test/BaseTest.java
+++ src/test/java/it/academy/test/BaseTest.java
@@ -2,19 +2,45 @@ package it.academy.test;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseTest {
 
-    public static WebDriver driver;
+//    public static WebDriver driver;
+
+    protected RemoteWebDriver driver;
+
+//    @RegisterExtension
+//    public SauceTestWatcher watcher = new SauceTestWatcher();
 
     @BeforeEach
-    public void setUp() {
-//        System.setProperty("webdriver.http.factory", "jdk-http-client");
-        driver = new ChromeDriver();
+    public void setUp(TestInfo testInfo) throws MalformedURLException {
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+
+        ChromeOptions options = new ChromeOptions();
+        options.setPlatformName("Windows 10");
+        options.setBrowserVersion("latest");
+
+        Map<String, Object> sauceOptions = new HashMap<>();
+        sauceOptions.put("username", System.getenv("SAUCE_USERNAME"));
+        sauceOptions.put("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
+        sauceOptions.put("name", testInfo.getDisplayName());
+
+        options.setCapability("sauce:options", sauceOptions);
+        URL url = new URL("https://oauth-vitalija.alisauskiene-96ae5:f5bfb19e-74aa-47f7-b004-c0b0b768f7b4@ondemand.eu-central-1.saucelabs.com:443/wd/hub");
+
+        driver = new RemoteWebDriver(url, options);
         driver.manage().window().maximize();
-        driver.get("http://localhost:3000/?#/");
+        driver.get("https://tomcat.akademijait.vtmc.lt/springyne/");
     }
 
     @AfterEach
