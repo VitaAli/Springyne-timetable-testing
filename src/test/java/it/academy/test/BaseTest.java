b0a085de373ed74a225ecec69372bf8ff2a0c837
Vitalija Ališauskienė <vitalija.alisauskiene@stud.vtmc.lt>
2023-03-24 17:27:54 +0200

    base test update

--- src/test/java/it/academy/test/BaseTest.java
+++ src/test/java/it/academy/test/BaseTest.java
@@ -11,6 +11,7 @@ public class BaseTest {
 
     @BeforeEach
     public void setUp() {
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
